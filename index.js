715e0cf7ad0f76b4ebf6fe5ccdb18cc71621e5d2
ilkerakkaya <ilkerakkaya@gmail.com>
2019-12-24 14:42:39 +0100

    Admin PAnel Changes

--- index.js
+++ index.js
@@ -212,6 +212,16 @@ app.get("/contacts", function(req,res){
     }
   });
 
+  app.post("/compose",function(req,res){
+
+    const post={
+      title:req.body.postTitle,
+      content:req.body.postBody
+    };
+    posts.push(post);
+    res.redirect("/");
+  });
+
 
 
 
